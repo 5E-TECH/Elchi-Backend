d84fda9a0f04c643db60c1c553baf52c10b1e2fa
FaxriddinDeveloperr <fazliddinomariipov@gmail.com>
2026-02-17 15:38:37 +0500

    Dilshodbek kalla qo'ygan xatoliklar to'girlandi

--- apps/identity-service/src/auth/dto/login.dto.ts
+++ apps/identity-service/src/auth/dto/login.dto.ts
@@ -1,14 +1,8 @@
-import { IsOptional, IsString, MinLength } from 'class-validator';
+import { IsString, MinLength } from 'class-validator';
 
 export class LoginDto {
-  @IsOptional()
   @IsString()
-  @MinLength(3)
-  username?: string;
-
-  @IsOptional()
-  @IsString()
-  phone_number?: string;
+  phone_number!: string;
 
   @IsString()
   @MinLength(4)
