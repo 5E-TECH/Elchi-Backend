4a5bc8ee890186a7c09e126dd82094cb52fc10ee
FaxriddinDeveloperr <fazliddinomariipov@gmail.com>
2026-02-23 17:20:38 +0500

    orde-service markets todat order get line

--- scripts/init-schemas.sql
+++ scripts/init-schemas.sql
@@ -2,22 +2,13 @@
 -- Bu script PostgreSQL server boshlanganda bir marta ishga tushiriladi
 
 CREATE SCHEMA IF NOT EXISTS identity_schema;
-GO
 CREATE SCHEMA IF NOT EXISTS order_schema;
-GO
 CREATE SCHEMA IF NOT EXISTS catalog_schema;
-GO
 CREATE SCHEMA IF NOT EXISTS logistics_schema;
-GO
 CREATE SCHEMA IF NOT EXISTS finance_schema;
-GO
 CREATE SCHEMA IF NOT EXISTS notification_schema;
-GO
 CREATE SCHEMA IF NOT EXISTS integration_schema;
-GO
 CREATE SCHEMA IF NOT EXISTS branch_schema;
-GO
 CREATE SCHEMA IF NOT EXISTS investor_schema;
-GO
 CREATE SCHEMA IF NOT EXISTS c2c_schema;
-GO
+CREATE SCHEMA IF NOT EXISTS search_schema;
